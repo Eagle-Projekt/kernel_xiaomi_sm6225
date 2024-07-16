0a55fe25477d8a2bd5e4d95f92c813f841de9617
Tanmsyffa <sltnmsyffa@gmail.com>
2024-07-16 22:40:58 +0700

    cpuidle: lpm-levels: Remove unused `cpu` variable at lpm_probe
    ../drivers/cpuidle/lpm-levels.c: In function 'lpm_probe':
    ../drivers/cpuidle/lpm-levels.c:773:22: warning: unused variable 'cpu' [-Wunused-variable]
      773 |         unsigned int cpu;
          |                      ^~~
    
    Signed-off-by: Tanmsyffa <sltnmsyffa@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1704,7 +1704,6 @@ static const struct platform_s2idle_ops lpm_s2idle_ops = {
 static int lpm_probe(struct platform_device *pdev)
 {
 	int ret;
-	unsigned int cpu;
 	struct hrtimer *cpu_histtimer;
 	struct kobject *module_kobj = NULL;
 
